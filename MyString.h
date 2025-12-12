bb93e4113bcdcf038436fa7836d0cf8395aa8b0c
Beihaqi Mariam <mbeihaqi@2D-LINUX-096.insa-lyon.fr>
2025-12-12 16:38:31 +0100

    Ajout des data members capacity et max_size

--- MyString.h
+++ MyString.h
@@ -13,19 +13,37 @@ class MyString {
   
 
   // Initialisation des fonctions 
+  // STUDENT A
+  // Fonction qui retourne le pointeur vers les caractères , Il fait en sorte à ce que la chaine est 
+  // toujours terminée par '\0'
   
+  void c_str(){
   
+  }
+  
+  void clear(){
+  
+  }
+  
+  void operator=(char){
+  
+  } 
+  
+  void operator+(const string&, const char*){
+  
+  }
   
 
   private: 
   size_t size_ ;
   // size() 
   // Fonction qui retourne le longeur du string en terme de bytes
-  char* string; // Initialisation of the string dans une array lié par des pointeurs
-  // Fonction qui retourne le pointeur vers les caractères , Il fait en sorte à ce que la chaine est 
-  // toujours terminée par '\0'
-  size_t capacity_ ;
+  char* string; // Initialisation of the string dans une array contingue
   
+  size_t capacity_ ;
+  // Variable qui permet d'allouer de l'espace memoire du string initile plus 1 => /0 == 0 en ASCII 
+  static size_t max_size_; 
+  // Variable de classe permettant de fixer l'espace memoire maximale pour allouer les string 
   
   //  Destructeur 
   ~MyString();
