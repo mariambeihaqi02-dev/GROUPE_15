37be82e5f69cce4062234a136ea352d5ee29f1f7
Omar Atiea <omkha01@gmail.com>
2026-01-10 14:29:28 +0100

    Student C : add my implemetation

--- String.cpp
+++ String.cpp
@@ -63,4 +63,97 @@ String& String::operator=(const char c ) { // Renvoie une référence vers l'obj
 
 
 
+//------------
+// Student C 
+//------------
+
+String::~String() {
+    delete[] data_;
+}
+
+size_t String::capacity() const {
+    return capacity_;
+}
+
+bool String::empty() const {
+    return size_ == 0;
+}
+
+
+void String::reserve(std::size_t n) {
+    if (n > max_size_) {
+        n=max_size_;
+    }
+    if (n <=capacity_) {
+        return;
+    }
+    char* new_string_ = new char[n + 1];
+    for (std::size_t i = 0; i < size_; ++i) {
+        new_string_[i] = data_[i];
+    }
+    new_string_[size_] = '\0';
+    delete[] data_;
+    data_ = new_string_;
+    capacity_ = n;
+}
+
+
+
+
+String& String::operator=(const char* s) {
+
+    if (s == nullptr) {
+        size_ = 0;
+        data_[0] ='\0';
+        return *this; 
+    }
+
+    std::size_t len = 0;
+    while (s[len]!= '\0' && len < 100) {
+        len++;
+    } 
+
+    if (len > capacity_) {
+        reserve(len);
+    }
+
+    for (std::size_t i = 0; i < len; i++) {
+        data_[i] = s[i];
+    }    
+    size_=len;
+    data_[size_]='\0';
+    return *this;
+}
+
+
+
+String operator+(const String& lhs, const String& rhs) {
+    String resultat; 
+    std::size_t total = lhs.size_ + rhs.size_;
+    if (total > 100) {
+        total = 100;
+    }
+    
+    if (total > resultat.capacity_) {
+        resultat.reserve(total);
+    }
+
+    std::size_t i = 0;
+    
+    while (i < lhs.size_ && i < total) {
+        resultat.data_[i] = lhs.data_[i];
+        i++;
+    }
+    std::size_t  j = 0;
+    while (j < rhs.size_ && i < total) {
+        resultat.data_[i] = rhs.data_[j];
+        i++;
+        j++;
+    }
+    resultat.size_ = i;
+    resultat.data_[ resultat.size_] = '\0';
+
+    return resultat;
+}
+
 
